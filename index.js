5ab124c8fe4ec5ce3ddce2b02f604eb575664ae5
Harshal <harshal.madgulkar725@gmail.com>
2025-09-23 16:46:33 +0530

    2. Print all elements from array except numbers greater than 2

--- index.js
+++ index.js
@@ -4,3 +4,12 @@
 // arr.forEach(function (val) {
 // 	console.log(`Hello ${val}`);
 // });
+
+// 2. Print all elements from array except numbers greater than 2
+// var arr = [1, 2, 3, 4, 5];
+
+// arr.forEach(function (val) {
+//     if (val < 2) {
+//         console.log(val);
+// 	}
+// });
