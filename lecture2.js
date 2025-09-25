6096e6ef088f7b41fe466290223011a97265e53f
Harshal <harshal.madgulkar725@gmail.com>
2025-09-25 16:34:34 +0530

    1. create function which will check passed parameter is Array or not (using Array.isArray)

---
+++ lecture2.js
@@ -0,0 +1,9 @@
+// 1. create function which will check passed parameter is Array or not
+// function checkArray(input) {
+// 	return Array.isArray(input);
+// }
+
+// console.log(checkArray(2));
+// console.log(checkArray({}));
+// console.log(checkArray([]));
+// console.log(checkArray([1, 2, 3, 4]));
