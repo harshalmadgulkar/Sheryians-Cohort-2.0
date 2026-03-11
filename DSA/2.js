049abf5e84b8ff8d06d2d0254336de6666567535
Harshal <harshal.madgulkar725@gmail.com>
2026-03-11 13:13:13 +0530

    refactor(dsa): comment out unused prompt code and enhance findFirstUnique function

--- DSA/2.js
+++ DSA/2.js
@@ -1,7 +1,31 @@
 // ! JS Essentials
-import createPrompt from "prompt-sync";
+// import createPrompt from "prompt-sync";
 
-const prompt = createPrompt();
+// const prompt = createPrompt();
 
-let age = prompt('Enter your age');
-console.log(age);
+// let age = prompt('Enter your age');
+// console.log(age);
+
+// Q.2
+// input string output => single char which is first non-repeated char from whole string
+
+function findFirstUnique(str) {
+    let exists = [];
+    let uniqueChar = [];
+
+    str.split("").forEach((char, index) => {
+        console.log(char);
+        let remainingString = str.slice(index + 1).split("");
+        if (!remainingString.includes(char) && !exists.includes(char)) {
+            console.log(remainingString);
+            uniqueChar.push(char);
+            return;
+        } else {
+            exists.push(char);
+        }
+    });
+    return uniqueChar[0];
+}
+
+const uniqueChar = findFirstUnique("aabbccdeff");
+console.log(uniqueChar);
